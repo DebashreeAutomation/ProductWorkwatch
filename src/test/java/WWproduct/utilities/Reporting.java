51c3a1076b7e09a23185772d8f26b4cd6352e876
DebashreeAutomation <p50038542@capita.co.uk>
2024-05-16 23:10:40 +0530

    change in pom file

--- src/test/java/WWproduct/utilities/Reporting.java
+++ src/test/java/WWproduct/utilities/Reporting.java
@@ -14,13 +14,15 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
 //import com.aventstack.extentreports.reporter.configuration.ChartLocation;
 //import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class Reporting extends TestListenerAdapter{
-	public ExtentSparkReporter sparkReporter;
+	public ExtentHtmlReporter htmlReporter;
 	public ExtentReports extent;
 	public ExtentTest logger;
 	
@@ -30,27 +32,23 @@ public class Reporting extends TestListenerAdapter{
 		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
 		String repName="Test-Report-"+timeStamp+".html";
 		
-		sparkReporter=new ExtentSparkReporter(System.getProperty("user.dir")+ "/test-output/"+repName);
-		try {
-			sparkReporter.loadXMLConfig(System.getProperty("user.dir")+ "/extent-config.xml");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		htmlReporter=new ExtentHtmlReporter(System.getProperty("user.dir")+ "/test-output/"+repName);
+		htmlReporter.loadXMLConfig(System.getProperty("user.dir")+ "/extent-config.xml/");
 		
 		
-		
-		sparkReporter.config().setDocumentTitle("WWProductAutomation"); 
-		sparkReporter.config().setReportName("Functional Test Automation Report"); 
-		//sparkReporter.config().setTestViewChartLocation(ChartLocation.TOP); 
-		sparkReporter.config().setTheme(Theme.DARK);
-		
+
         extent=new ExtentReports();
-		extent.attachReporter(sparkReporter);
+		
+		extent.attachReporter(htmlReporter);
 		extent.setSystemInfo("Project name","WWProductAutomation");
 		extent.setSystemInfo("Host name","localhost");
-		extent.setSystemInfo("Environemnt","INT");
+		extent.setSystemInfo("Environemnt","QA");
 		extent.setSystemInfo("Tester","debashree");
+		
+		htmlReporter.config().setDocumentTitle("WWProductAutomation"); 
+		htmlReporter.config().setReportName("Functional Test Automation Report"); 
+		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP); 
+		htmlReporter.config().setTheme(Theme.DARK);
 	}
 	
 	public void onTestSuccess(ITestResult tr)
@@ -61,17 +59,23 @@ public class Reporting extends TestListenerAdapter{
 	
 	public void onTestFailure(ITestResult tr)
 	{
-		logger=extent.createTest(tr.getName()); 
-		logger.log(Status.FAIL,MarkupHelper.createLabel(tr.getName(),ExtentColor.RED));
-		
-		String screenshotsPath=System.getProperty("user.dir")+"/screenshotFolder/"+tr.getName()+".png";
-		
-		File f = new File(screenshotsPath); 
-		
-		if(f.exists())
-		{
+	logger=extent.createTest(tr.getName()); 
+	logger.log(Status.FAIL,MarkupHelper.createLabel(tr.getName(),ExtentColor.RED));
+	
+	String screenshotsPath=System.getProperty("user.dir")+"/screenshotFolder/"+tr.getName()+".png";
+	
+	File f = new File(screenshotsPath); 
+	
+	if(f.exists())
+	{
+	try {
 		logger.fail("Screenshot is below:" + logger.addScreenCaptureFromPath(screenshotsPath));
-		}
+		} 
+	catch (IOException e) 
+			{
+			e.printStackTrace();
+			}
+	}
 		
 	}
 	
