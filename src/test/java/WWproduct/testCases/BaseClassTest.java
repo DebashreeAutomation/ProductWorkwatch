3497474ac333575b5de668f52f5fda71b7f22be1
DebashreeAutomation <p50038542@capita.co.uk>
2024-05-28 23:43:31 +0530

    change

--- src/test/java/WWproduct/testCases/BaseClassTest.java
+++ src/test/java/WWproduct/testCases/BaseClassTest.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.RandomStringUtils;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -85,17 +85,6 @@ public class BaseClassTest {
 			File target = new File(System.getProperty("user.dir") + "\\screenshotFolder\\" + tname + ".png");
 			FileUtils.copyFile(source, target);
 			System.out.println("Screenshot taken");
-		}
-		
-		public String randomestring()
-		{
-			String generatedstring=RandomStringUtils.randomAlphabetic(8);
-			return(generatedstring);
-		}
-		
-		public static String randomeNum() {
-			String generatedString2 = RandomStringUtils.randomNumeric(4);
-			return (generatedString2);
 		}
 		
 		
